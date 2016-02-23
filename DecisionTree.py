574c15168e2dd6ebe96655d9f95fd9e444daf063
yifeiliu <cdliuyifei@jd.com>
2016-02-23 10:32:41 +0800

    init decision tree & random forest models with default parameters

---
+++ DecisionTree.py
@@ -0,0 +1,19 @@
+
+from pyspark import SparkConf, SparkContext, mllib
+from pyspark.mllib.tree import DecisionTree
+
+
+def trainModel(trainingData):
+	model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={}, impurity='gini', maxDepth=5,maxBins=32)
+	print '\nTraining DecisionTree model finished'
+	return model
+
+
+def evaluateModel(model, testData):
+	predictions = model.predict(testData.map(lambda item: item.features))
+	labelsAndPredictions = testData.map(lambda item: item.label).zip(predictions)
+	testErr = labelsAndPredictions.filter(lambda (v,p): v != p).count()/float(testData.count())
+
+	print "\nDecisionTree Model Evaluation"
+	print "\nTest Error = " + str(testErr)
+
---
+++ RandomForest.py
@@ -0,0 +1,20 @@
+
+from pyspark import SparkConf, SparkContext, mllib
+from pyspark.mllib.tree import RandomForest
+
+def trainModel(trainingData):
+	model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={}, 
+											numTrees=3, featureSubsetStrategy="auto", impurity='gini',
+											maxDepth=5, maxBins=32)
+
+	print '\nTraining RandomForest model finished'
+	return model
+
+
+def evaluateModel(model, testData):
+	predictions = model.predict(testData.map(lambda item: item.features))
+	labelsAndPredictions = testData.map(lambda item: item.label).zip(predictions)
+	testErr = labelsAndPredictions.filter(lambda (v,p): v != p).count()/float(testData.count())
+
+	print "\RandomForest Model Evaluation"
+	print "\nTest Error = " + str(testErr)
