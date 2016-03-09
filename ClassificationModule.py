87979897b46a8f5b6a726e82e19a476d48f87260
yifeiliu <cdliuyifei@jd.com>
2016-03-09 11:42:34 +0800

    remove virtual part from each record in eigen vector file

--- ClassificationModule.py
+++ ClassificationModule.py
@@ -4,5 +4,20 @@ import pandas as pd
 import Utils
 
 def process(sc, eigenVecFile, markedClusterFile):
-	eigenVec = sc.textFile(eigenVecFile)
-	print eigenVec.take(3)
+	eigenVecRDD = sc.textFile(eigenVecFile)
+	#print eigenVecRDD.take(3)
+	filteredEigenVecRDD = eigenVecRDD.map(lambda item: removeVirtualPart(item))
+	#print filteredEigenVecRDD.take(3)
+
+
+def removeVirtualPart(item):
+	return [extract(sub) for sub in item.split(',')]
+
+
+def extract(item):
+	left = item.find('(')
+	right = item.find('+')
+	if left != -1 and right != -1:
+		return float(item[left+1:right])
+	else:
+		return 0.0
