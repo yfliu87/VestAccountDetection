f8b44e3567ed60d03144d9658176600bb1edf57d
yifeiliu <cdliuyifei@jd.com>
2016-03-10 11:42:04 +0800

    return sse and error value

--- Evaluation.py
+++ Evaluation.py
@@ -1,8 +1,11 @@
 #-*-coding:utf-8-*-
 
 def evaluateModel(model, rdd):
-	print "SSE: " + str(model.computeCost(rdd))
+	sse = model.computeCost(rdd)
+	print "\nSSE: " + str(sse)
+	return sse
 
 def calculateErrorRate(rdd):
 	error = rdd.filter(lambda (v,p): v != p).count()/float(rdd.count())
 	print "\nError rate: " + str(error)
+	return error
