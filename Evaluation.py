fe2c301dd5a626f0bbb25d78da62d8d622e4b7a7
yifeiliu <cdliuyifei@jd.com>
2016-03-11 18:32:21 +0800

    self define error method for accuracy calculation

--- Evaluation.py
+++ Evaluation.py
@@ -5,7 +5,16 @@ def evaluateModel(model, rdd):
 	print "\nSSE: " + str(sse)
 	return sse
 
+def selfDefinedError(label, prediction):
+	if label == prediction:
+		return (False)
+	elif (label == 3 and prediction == 2) or (label == 2 and prediction == 3) or (label == 1 and prediction ==0) or (label == 0 and prediction == 1):
+		return (False)
+	else:
+		return (True)
+
 def calculateErrorRate(rdd):
-	error = rdd.filter(lambda (v,p): v != p).count()/float(rdd.count())
-	print "\nError rate: " + str(error)
+	error = rdd.filter(lambda (v,p): selfDefinedError(v,p)).count()/float(rdd.count())
+	#print "\nError rate: " + str(error)
 	return error
+
