3415d6a2961ed82ae53e9c5d82c9c28aab2c4bf1
yifeiliu <cdliuyifei@jd.com>
2016-03-15 11:14:11 +0800

    refactor cluster main for demo use

--- ClusterMain.py
+++ ClusterMain.py
@@ -6,10 +6,25 @@ import ComputeModule as cm
 import PredefinedValues as pv
 import Evaluation as eva
 import ClassificationModule as classification
+import Utils
+
+optimalClusterModel = None
+optimalClassificationModel = None
+minTrainingError = 1.0
+minTestError = 1.0
+minSSE = 1.0
+writer = open('/home/yifei/TestData/data/realdata/performanceParam.csv', 'w')
+sparkContext = SparkContext()
+optimalRecord = None
+optimalClusterNum = None
+optimalDimension = None
+optimalDepth = None
+optimalBin = None
+
 
 def run():
 	#truncate raw data to managable amount
-	fp.truncate(pv.sourceFile, pv.truncatedFile, pv.truncateLineCount)
+	fp.truncate(pv.mergedAccountFile, pv.truncatedFile, pv.truncateLineCount)
 
 	fp.preprocess(pv.truncatedFile, pv.processedFile, pv.targetFields)
 
@@ -25,9 +40,62 @@ def run():
 
 	fp.outputNodesInSameCluster(model, unifiedRDDVecs, rawDataFrame, pv.clusterIDCenterFile, pv.clusterIDFile)
 
-	classification.process(sparkContext, pv.clusterNum, pv.eigenVecFile, pv.clusterIDFile)
+	#classification.process(sparkContext, pv.clusterNum, pv.treeMaxDepth, pv.treeMaxBins, pv.eigenVecFile, pv.clusterIDFile)
 
+	Utils.logMessage("\nTrain cluster model finished")
+
+def run(recordCount, clusterNum, dimension, maxDepth, maxBin):
+	global sparkContext, writer, optimalClusterModel, optimalClassificationModel, minTrainingError, minTestError, minSSE,optimalRecord, optimalClusterNum, optimalDimension, optimalDepth, optimalBin
+	
+	#fp.truncate(pv.sourceFile, pv.truncatedFile, recordCount)
+	fp.truncate(pv.mergedAccountFile, pv.truncatedFile, recordCount)
+
+	fp.preprocess(pv.truncatedFile, pv.processedFile, pv.targetFields)
+
+	rawDataFrame = fp.readData(pv.trainingFile)
+
+	simMat = cm.getSimilarityMatrixMultiProcess(rawDataFrame)
+
+	#sparkContext = SparkContext()
+
+	clusterModel, unifiedRDDVecs = cluster.getClusterModel(sparkContext, simMat, rawDataFrame, clusterNum, dimension, pv.eigenVecFile)
+
+	sse = eva.evaluateModel(clusterModel, unifiedRDDVecs)
+
+	fp.outputNodesInSameCluster(clusterModel, unifiedRDDVecs, rawDataFrame, pv.clusterIDCenterFile, pv.clusterIDFile)
+
+	classificationModel, trainingError, testError = classification.process(sparkContext, clusterNum, maxDepth, maxBin, pv.eigenVecFile, pv.clusterIDFile)
+
+	writer.write("\nRun with %s record, %s clusters, %s dimensions, %s tree depth, %s bins" %(str(recordCount), str(clusterNum), str(dimension), str(maxDepth), str(maxBin)))
+	writer.write("\n\tcluster sse: %s, classification training err: %s, test err: %s" %(str(sse) ,str(trainingError), str(testError)))
+
+	if trainingError < minTrainingError and testError < minTestError:
+		minTrainingError = trainingError
+		minTestError = testError
+		optimalClusterModel = clusterModel
+		optimalClassificationModel = classificationModel
+		minSSE = sse
+		optimalRecord = recordCount
+		optimalClusterNum = clusterNum
+		optimalDimension = dimension
+		optimalDepth = maxDepth
+		optimalBin = maxBin
 
 if __name__ == '__main__':
-	run()
+	global sparkContext, writer, optimalClusterModel, optimalClassificationModel, minTrainingError, minTestError, minSSE,optimalRecord, optimalClusterNum, optimalDimension, optimalDepth, optimalBin
+
+	for recordCount in [10001]:
+		for clusterNum in [300]:
+			for dimension in [12]:
+				for maxDepth in [5]:
+					for maxBin in [32]:
+						run(recordCount, clusterNum, dimension, maxDepth, maxBin)
+
+		writer.write('\nCurrent run optimal param, record %s, cluster %s, dimension %s, maxDepth %s, maxBin %s' %(str(optimalRecord), str(optimalClusterNum), str(optimalDimension), str(optimalDepth), str(optimalBin)))
+	writer.write("\n\toptimal cluster sse: " + str(minSSE) + ', classification training err: ' + str(minTrainingError) + ', test err: ' + str(minTestError))
+	writer.write('\n\nFinal optimal param\nrecord %s, cluster %s, dimension %s, maxDepth %s, maxBin %s' %(str(optimalRecord), str(optimalClusterNum), str(optimalDimension), str(optimalDepth), str(optimalBin)))
+	writer.close()
+	optimalClusterModel.save(sparkContext, '/home/yifei/TestData/data/realdata/cluster.model')
+	#optimalClassificationModel.save(sparkContext,'/home/yifei/TestData/data/realdata/classificationModel.mod')
+	#run()
 	
