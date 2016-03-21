8bdc7799a3b4f4c024eb35edcf00169a25d3854e
yifeiliu <cdliuyifei@jd.com>
2016-03-21 11:08:54 +0800

    delete unnecessary code

--- ClassificationMain.py
+++ ClassificationMain.py
@@ -14,12 +14,6 @@ import Evaluation as eva
 import PredefinedValues as pv
 import Utils
 
-#sparkContext = SparkContext()
-#writer = open('/home/yifei/TestData/data/realdata/classification_50000.csv','w')
-optimalModel = None
-minTrainingError = 1.0
-minTestError = 1.0
-
 
 def preprocess():
 	
@@ -85,11 +79,7 @@ def train(sparkContext):
 	truncatedAccounts = sparkContext.textFile(pv.processedFile).take(pv.truncateLineCount - 1)
 	rawData = sparkContext.parallelize(truncatedAccounts).map(countByFeatures).map(lambda item: LabeledPoint(item[0], Vectors.dense(item[2:])))
 
-	for ratio in [0.7]:
-		for impurity in ['entropy']:
-			for maxDepth in [4]:
-				for maxBin in [16]:
-					trainWithParam(sparkContext, rawData, ratio, impurity, maxDepth, maxBin)
+	trainWithParam(sparkContext, rawData, 0.7, 'entropy', 4, 16)
 
 
 def trainWithParam(sparkContext, rawData, ratio, impurity, maxDepth, maxBin):
@@ -99,33 +89,6 @@ def trainWithParam(sparkContext, rawData, ratio, impurity, maxDepth, maxBin):
 	print '\nDecision Tree Training Err: %s, Test Err: %s' %(str(trainingError), str(testError))
 	decisionTreeModel.save(sparkContext, pv.classificationModelPath)
 
-	'''
-	writer.write('\nCurrent run ratio %s, maxDepth %s, maxBin %s, impurity %s' %(str(ratio), str(maxDepth), str(maxBin), impurity))
-	writer.write('\n\tDecision Tree TrainingError %s, TestError %s' %(str(trainingError), str(testError)))
-	recordOptimal(trainingError, testError, decisionTreeModel)
-
-	randomForestModel, trainingError, testError = RandomForestProcess(trainingSet, testSet, impurity, maxDepth, maxBin)
-	print '\nRandom Forest Training Err: %s, Test Err: %s' %(str(trainingError), str(testError))
-
-	naiveBayesModel, trainingError, testError = NaiveBayesProcess(trainingSet, testSet)
-	print '\nNaive Bayes Training Err: %s, Test Err: %s' %(str(trainingError), str(testError))
-
-	#writer.write('\n\tNaive Bayes TrainingError %s, TestError %s' %(str(trainingError), str(testError)))
-	#recordOptimal(trainingError, testError, decisionTreeModel)
-
-	logRegressionModel, trainingError, testError = LogisticRegressionProcess(trainingSet, testSet)
-	print '\nLogistic Regression Training Err: %s, Test Err: %s' %(str(trainingError), str(testError))
-
-	#writer.write('\n\tLogistic Regression TrainingError %s, TestError %s' %(str(trainingError), str(testError)))
-	#recordOptimal(trainingError, testError, logRegressionModel)
-
-	#binary classification
-	SVMModel, trainingError, testError = SVMProcess(trainingSet, testSet)
-	print '\nSVM Training Err: %s, Test Err: %s' %(str(trainingError), str(testError))
-
-	#writer.write('\n\tSVM TrainingError %s, TestError %s' %(str(trainingError), str(testError)))
-	#recordOptimal(trainingError, testError, SVMModel)
-	'''
 
 def countByFeatures(item):
 	items = item.split(',')
@@ -135,14 +98,6 @@ def countByFeatures(item):
 	promotionNum = len(items[4].split('|'))
  
 	return (items[-1], items[0], ipNum ** 2, devIDNum ** 3, addrNum ** 2, promotionNum)
-
-
-def recordOptimal(trainingError,testError, model):
-	global minTrainingError, minTestError, optimalModel
-	if trainingError < minTrainingError and testError < minTestError:
-		minTrainingError = trainingError
-		minTestError = testError
-		optimalModel = model 
 
 
 def DecisionTreeProcess(trainingSet, testSet, imp, dtMaxDepth, dtMaxBins):
@@ -161,73 +116,6 @@ def DecisionTreeProcess(trainingSet, testSet, imp, dtMaxDepth, dtMaxBins):
 
 	return decisionTreeModel, trainingError, testError
 
-def RandomForestProcess(trainingSet, testSet, imp, dtMaxDepth, dtMaxBins):
-	randomForestModel = RandomForest.trainClassifier(trainingSet, numClasses = 2, categoricalFeaturesInfo={},
-														numTrees = 3, featureSubsetStrategy="auto",
-														impurity=imp,maxDepth=dtMaxDepth, maxBins=dtMaxBins)
-
-	predictions = randomForestModel.predict(trainingSet.map(lambda item: item.features))
-	trainingLabelsAndPredictions = trainingSet.map(lambda item: item.label).zip(predictions)
-	trainingError = eva.calculateErrorRate(trainingLabelsAndPredictions)
-
-	predictions = randomForestModel.predict(testSet.map(lambda item: item.features))
-	testLabelsAndPredictions = testSet.map(lambda item: item.label).zip(predictions)
-	testError = eva.calculateErrorRate(testLabelsAndPredictions)
-
-	return randomForestModel, trainingError, testError
-
-def NaiveBayesProcess(trainingSet, testSet):
-	naiveBayesModel = NaiveBayes.train(trainingSet)
-
-	predictions = naiveBayesModel.predict(trainingSet.map(lambda item: item.features))
-	trainingLabelsAndPredictions = trainingSet.map(lambda item: item.label).zip(predictions)
-	trainingError = eva.calculateErrorRate(trainingLabelsAndPredictions)
-
-	predictions = naiveBayesModel.predict(testSet.map(lambda item: item.features))
-	testLabelsAndPredictions = testSet.map(lambda item: item.label).zip(predictions)
-	testError = eva.calculateErrorRate(testLabelsAndPredictions)
-
-	return naiveBayesModel, trainingError, testError
-
-
-def LogisticRegressionProcess(trainingSet, testSet):
-	logRegressionModel = LogisticRegressionWithLBFGS.train(trainingSet)
-
-	predictions = logRegressionModel.predict(trainingSet.map(lambda item: item.features))
-	trainingLabelsAndPredictions = trainingSet.map(lambda item: item.label).zip(predictions)
-	trainingError = eva.calculateErrorRate(trainingLabelsAndPredictions)
-
-	predictions = logRegressionModel.predict(testSet.map(lambda item: item.features))
-	testLabelsAndPredictions = testSet.map(lambda item: item.label).zip(predictions)
-	testError = eva.calculateErrorRate(testLabelsAndPredictions)
-
-	return logRegressionModel, trainingError, testError
-
-
-def SVMProcess(trainingSet, testSet):
-	
-	SVMModel = SVMWithSGD.train(trainingSet, iterations=100)
-
-	predictions = SVMModel.predict(trainingSet.map(lambda item: item.features))
-	trainingLabelsAndPredictions = trainingSet.map(lambda item: item.label).zip(predictions)
-	trainingError = eva.calculateErrorRate(trainingLabelsAndPredictions)
-
-	predictions = SVMModel.predict(testSet.map(lambda item: item.features))
-	testLabelsAndPredictions = testSet.map(lambda item: item.label).zip(predictions)
-	testError = eva.calculateErrorRate(testLabelsAndPredictions)
-
-	return SVMModel, trainingError, testError
-
-
 
 if __name__ == '__main__':
 	pass
-	#global writer, optimalModel, minTrainingError, minTestError
-
-	#preprocess()
-
-	#train()
-
-	#writer.write('\n\nFinal optimal param\nminTrainingError %s, minTestError %s' %(str(minTrainingError), str(minTestError)))
-	#writer.close()
-	#optimalModel.save(sparkContext,'/home/yifei/TestData/data/realdata/classification.model')
--- ClusterMain.py
+++ ClusterMain.py
@@ -14,8 +14,6 @@ optimalClassificationModel = None
 minTrainingError = 1.0
 minTestError = 1.0
 minSSE = 1.0
-#writer = open('/home/yifei/TestData/data/realdata/performanceParam.csv', 'w')
-#sparkContext = SparkContext()
 optimalRecord = None
 optimalClusterNum = None
 optimalDimension = None
@@ -24,18 +22,14 @@ optimalBin = None
 
 
 def train(sparkContext):
-	#truncate raw data to managable amount
 	fp.truncate(pv.mergedAccountFile, pv.truncatedFile, pv.truncateLineCount)
 
 	fp.preprocess(pv.truncatedFile, pv.processedFile, pv.targetFields)
 
-	#rawDataFrame = fp.readData(pv.trainingFile)
-	#simMat = cm.getSimilarityMatrixMultiProcess(rawDataFrame)
-
 	pd.read_csv(pv.trainingFile, sep=',',encoding='utf-8').to_csv(pv.fileForClusterModel, index=False, encoding='utf-8')
 
 	rawDataFrame = pd.read_csv(pv.fileForClusterModel, sep=',',encoding='utf-8')
-	
+
 	simMat = cm.getSimilarityMatrix(sparkContext, rawDataFrame)
 
 	model, unifiedRDDVecs = cluster.getClusterModel(sparkContext, simMat, rawDataFrame, pv.clusterNum, pv.dimensionReductionNum, pv.eigenVecFile)
@@ -49,60 +43,8 @@ def train(sparkContext):
 	model.save(sparkContext, pv.clusterModelPath)
 
 	Utils.logMessage("\nTrain cluster model finished")
-	Utils.logMessage("\nCluster precision: \nTraining error %s , Test error %s" %(str(trainingError), str(testError)))
+	Utils.logMessage("\nCluster precision: \n\tTraining error %s , Test error %s" %(str(trainingError), str(testError)))
 
-'''
-def train(recordCount, clusterNum, dimension, maxDepth, maxBin):
-	global writer, optimalClusterModel, optimalClassificationModel, minTrainingError, minTestError, minSSE,optimalRecord, optimalClusterNum, optimalDimension, optimalDepth, optimalBin
-	
-	#fp.truncate(pv.sourceFile, pv.truncatedFile, recordCount)
-	fp.truncate(pv.mergedAccountFile, pv.truncatedFile, recordCount)
-
-	fp.preprocess(pv.truncatedFile, pv.processedFile, pv.targetFields)
-
-	rawDataFrame = fp.readData(pv.trainingFile)
-
-	simMat = cm.getSimilarityMatrixMultiProcess(rawDataFrame)
-
-	#sparkContext = SparkContext()
-
-	clusterModel, unifiedRDDVecs = cluster.getClusterModel(sparkContext, simMat, rawDataFrame, clusterNum, dimension, pv.eigenVecFile)
-
-	sse = eva.evaluateModel(clusterModel, unifiedRDDVecs)
-
-	fp.outputNodesInSameCluster(clusterModel, unifiedRDDVecs, rawDataFrame, pv.clusterIDCenterFile, pv.clusterIDFile)
-
-	classificationModel, trainingError, testError = classification.process(sparkContext, clusterNum, maxDepth, maxBin, pv.eigenVecFile, pv.clusterIDFile)
-
-	writer.write("\nRun with %s record, %s clusters, %s dimensions, %s tree depth, %s bins" %(str(recordCount), str(clusterNum), str(dimension), str(maxDepth), str(maxBin)))
-	writer.write("\n\tcluster sse: %s, classification training err: %s, test err: %s" %(str(sse) ,str(trainingError), str(testError)))
-
-	if trainingError < minTrainingError and testError < minTestError:
-		minTrainingError = trainingError
-		minTestError = testError
-		optimalClusterModel = clusterModel
-		optimalClassificationModel = classificationModel
-		minSSE = sse
-		optimalRecord = recordCount
-		optimalClusterNum = clusterNum
-		optimalDimension = dimension
-		optimalDepth = maxDepth
-		optimalBin = maxBin
 
 if __name__ == '__main__':
-	global writer, optimalClusterModel, optimalClassificationModel, minTrainingError, minTestError, minSSE,optimalRecord, optimalClusterNum, optimalDimension, optimalDepth, optimalBin
-
-	for recordCount in [10001]:
-		for clusterNum in [300]:
-			for dimension in [12]:
-				for maxDepth in [5]:
-					for maxBin in [32]:
-						train(recordCount, clusterNum, dimension, maxDepth, maxBin)
-
-		writer.write('\nCurrent run optimal param, record %s, cluster %s, dimension %s, maxDepth %s, maxBin %s' %(str(optimalRecord), str(optimalClusterNum), str(optimalDimension), str(optimalDepth), str(optimalBin)))
-	writer.write("\n\toptimal cluster sse: " + str(minSSE) + ', classification training err: ' + str(minTrainingError) + ', test err: ' + str(minTestError))
-	writer.write('\n\nFinal optimal param\nrecord %s, cluster %s, dimension %s, maxDepth %s, maxBin %s' %(str(optimalRecord), str(optimalClusterNum), str(optimalDimension), str(optimalDepth), str(optimalBin)))
-	writer.close()
-	optimalClusterModel.save(sparkContext, '/home/yifei/TestData/data/realdata/cluster.model')
-	#optimalClassificationModel.save(sparkContext,'/home/yifei/TestData/data/realdata/classificationModel.mod')
-'''
+	pass
